df2d372d3d5dec5e607f24ec1decc058f7b8ac67
Khushboo <r1khushboo1996@gmail.com>
2022-08-22 09:17:37 +0530

    login page changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -44,12 +44,28 @@ export class LoginComponent implements OnInit {
         this.service.login(this.email,this.password, this.mailid).subscribe((d)=>{
          if(d == true){
             if(this.retUrl!=null){
-
-            // this.sar.getCartDetails().subscribe((data)=>{
-            //   console.log(data);
-            //   this.cartPending = data;
               
-            // });
+              localStorage.setItem('email', this.email);
+              this.sar.getCartDetails().subscribe((cartdata)=>{
+                this.cartPending = cartdata;
+                var cartd = this.cartPending.filter((x: { status: string; })=>x.status == "pending");
+                
+                for(var i=0; i<cartd.length; i++)
+                {
+                    var cartInfo = cartd[i];
+                    var updateJsonData ={
+                      email: localStorage.getItem('email'),
+                      id:cartInfo.id,
+                      itemName: cartInfo.itemName,
+                      price : cartInfo.price,
+                      totalPrice : cartInfo.quntity * cartInfo.price,
+                      quntity: cartInfo.quntity,
+                      status:"Delivery Pending"
+                  }
+                  this.deleteCartDetails(updateJsonData);
+                  
+               }
+              });
 
               this.router.navigate([this.retUrl]);
               
